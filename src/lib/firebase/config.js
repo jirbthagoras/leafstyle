a4a6069e89edb0228f0210c887293cfacb9d2532
jirbthagoras <hansjabriel@gmail.com>
2024-12-21 11:09:58 +0700

    [+] Store to Firebase

--- src/lib/firebase/config.js
+++ src/lib/firebase/config.js
@@ -1,11 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
 
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+export const db = getFirestore(app);
 
 export { auth };
--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,8 +1,17 @@
-import { auth } from "@/lib/firebase/config";
+import { auth, db } from "@/lib/firebase/config";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 export const signUpUser = async (email: string, password: string) => {
   const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+  const userRef = doc(db, "users", userCredential.user.uid); // Use UID as document ID
+  await setDoc(userRef, {
+    points: 0,
+    treesGrown: 0,
+    lastUpdated: serverTimestamp(),
+  });
+
   return userCredential.user;
 };
 
